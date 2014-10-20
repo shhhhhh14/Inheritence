981dec779fccf0432f2866acd1252858e47f3276
shhhhhh14 <shauna.dowling1@gmail.com>
2014-10-20 22:36:40 +0100

    Update volunteer.rb

--- volunteer.rb
+++ volunteer.rb
@@ -4,10 +4,7 @@ class Volunteer < Manager #subclass is volunteer inheriting from manager
 
   def initialize(name, charity, hours, pay, bonus, pps_number)
     super name, hours, bonus, pay, pps_number #super calls the method of the parent class Manager
-    @pay = pay = 0                       #pay is always initialized as 0 and calculated new for each new wage
+    @pay = pay = 0                       #pay is always initialized at 0 and calculated new for each new wage
     @charity = charity                   #the charity the volunteer works for
   end
-
-
-
 end
